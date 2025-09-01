e77ce086b7b360b3af4f19339e4b281799fd29ff
sangderenard <47421372+sangderenard@users.noreply.github.com>
2025-08-31 22:21:48 -0500

    feat: add sgd optimizer and disable bpid option

--- tests/test_bpid_sgd.py
+++ tests/test_bpid_sgd.py
@@ -1,4 +1,4 @@
-from src.common.tensors.abstract_nn.optimizer import BPIDSGD
+from src.common.tensors.abstract_nn.optimizer import BPIDSGD, SGD
 from src.common.tensors.abstraction import AbstractTensor as AT
 
 
@@ -12,3 +12,11 @@ def test_bpid_sgd_integral_accumulation():
     new_p = opt.step([p], [g])[0]
     AT.copyto(p, new_p)
     assert p.tolist() == [-3.0]
+
+
+def test_sgd_step():
+    p = AT.get_tensor([0.0])
+    g = AT.get_tensor([1.0])
+    opt = SGD([p], lr=0.1)
+    new_p = opt.step([p], [g])[0]
+    assert new_p.tolist() == [-0.1]
